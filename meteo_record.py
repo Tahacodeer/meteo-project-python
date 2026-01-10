ec139885204d1b0eb843b775df8f0c92be919dc6
rihane_hamza <rihane.h813@ucd.ac.ma>
2026-01-10 01:26:43 +0100

    CREATED class : Meteo_record

--- meteo_record.py
+++ meteo_record.py
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
 
 
     
+=======
+class Meteo_record:
+    def __init__(self,date,temperature,humidity,rainfall):
+        self.__date=date
+        self.__temperature=temperature
+        self.__humidity=humidity
+        self.__rainfall=rainfall
+    @property
+    def date:
+        return self.__date
+    @property
+    def temperature:
+        return self.__temperature
+    @property
+    def rainfall:
+        return self.__rainfall
+    def __repr__(self):
+        return f"la date est : {self.__date!r}  la temperature est : {self.temperature!r}  la humidity est :{self.humidity!r}  la rainfall est : {self.rainfall!r}"
+    def __self__(self):
+        return f"la date est : {self.__dater} \n la temperature est : {self.temperature} \n la humidity est :{self.humidity} \n la rainfall est : {self.rainfall}"
+"
+    
+>>>>>>> eeb41b4 (created class Meteo_record)
